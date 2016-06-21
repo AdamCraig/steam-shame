9dd87a4833a9bf526fe43f8877b972f253e56ebc
Adam Craig and Ashley Maceli <adam@>
2016-06-21 16:44:35 -0700

    Remove content variable to enable proper building

--- app/routes/userid-results.js
+++ app/routes/userid-results.js
@@ -15,31 +15,4 @@ export default Ember.Route.extend({
       }),
     });
   }
-
-  content: [
-	{
-		"label": "Equity",
-		"value": 12935781.176999997
-	},
-	{
-		"label": "Real Assets",
-		"value": 10475849.276172025
-	},
-	{
-		"label": "Fixed Income",
-		"value": 8231078.16438347
-	},
-	{
-		"label": "Cash & Cash Equivalent",
-		"value": 5403418.115000006
-	},
-	{
-		"label": "Hedge Fund",
-		"value": 1621341.246006786
-	},
-	{
-		"label": "Private Equity",
-		"value": 1574677.59
-	}
-  ],
 });
