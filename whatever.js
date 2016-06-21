c3fd67f78af56f4f9674d166e7e34aea9d6010a4
Noah Kittleson <noahkittleson@gmail.com>
2016-06-21 09:39:18 -0700

    took apiKey out of whatever.js

--- whatever.js
+++ whatever.js
@@ -1,10 +1,12 @@
+require('dotenv').load();
+var apiKey = process.env.apiKey;
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var app = express();
 
 app.get('/steam/fuckthepolice', function(httpRequest, httpResponse) {
-  var url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=D6A3A9EB246EE79A6466E65E71CC7C89&steamids=76561198025036440';
+  var url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + apiKey + '&steamids=76561198025036440';
   request.get(url, function(error, steamHttpResponse, steamHttpBody) {
     httpResponse.setHeader('Content-Type', 'application/json');
     httpResponse.setHeader('Access-Control-Allow-Origin', '*');
@@ -15,7 +17,7 @@ app.get('/steam/fuckthepolice', function(httpRequest, httpResponse) {
 app.get('/steam/civ5achievements', function(httpRequest, httpResponse) {
     // Calculate the Steam API URL we want to use
     var url = 'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/' +
-        'v0002/?appid=440&key=D6A3A9EB246EE79A6466E65E71CC7C89&steamid=76561198025036440';
+        'v0002/?appid=440&key='+ apiKey +'&steamid=76561198025036440';
     request.get(url, function(error, steamHttpResponse, steamHttpBody) {
 
         // Once we get the body of the steamHttpResponse, send it to our client
@@ -30,10 +32,11 @@ app.get('/steam/civ5achievements', function(httpRequest, httpResponse) {
 app.get('/steam/game/:appid/achievements', function(httpRequest, httpResponse) {
     // Calculate the Steam API URL we want to use
     var url = 'http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/' +
-        'v2/?key=D6A3A9EB246EE79A6466E65E71CC7C89&appid=' +
+        'v2/?key=' + apiKey + '&appid=' +
         httpRequest.params.appid;
     request.get(url, function(error, steamHttpResponse, steamHttpBody) {
         httpResponse.setHeader('Content-Type', 'application/json');
+        httpResponse.setHeader('Access-Control-Allow-Origin', '*');
         httpResponse.send(steamHttpBody);
     });
 });
