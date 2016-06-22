15a6828a6c639493be0d940bed7e6b19a4da4a37
Adam Craig and Ashley Maceli <ashley@>
2016-06-22 14:28:47 -0700

    Add working function for converting an entered game name into a proper app search

--- app/routes/games.js
+++ app/routes/games.js
@@ -1,23 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
-    // var appid;
-    return Ember.$.getJSON('http://localhost:4000/steam/games').then(function(response){
+  model() {
+    var allGamesURL = 'http://localhost:4000/steam/games';
+
+    return Ember.$.getJSON(allGamesURL).then(function(response) {
       var list = response.applist.apps.app;
-      console.log(list);
-      var findAppIDFromTitle = function(title){
+      var foundGameID;
+
+      var getGameInfo = function(title){
         list.forEach(function(item) {
           if (item.name === title) {
-            console.log("I FOUND IT!" + item.appid);
-
+            foundGameID = item.appid;
           }
-          // this is an alternative to find sub-title
-          // if (item.name.split(':')[1].trim() === title) appid =  item.appid;
-        })
+        });
+        return Ember.$.getJSON('http://localhost:4000/steam/game/' + foundGameID + '/achievements').then(function(response) {
+          console.log(response.game.availableGameStats.achievements);
+          return response.game.availableGameStats.achievements;
+        });
       };
-      findAppIDFromTitle('Team Fortress 2');
-
+      return getGameInfo('Bastion');
     });
   },
+
 });
