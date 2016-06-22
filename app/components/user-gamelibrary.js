94bb98ddbf84b429a00ac9f27495997e6f9532e4
Noah Kittleson <noahkittleson@gmail.com>
2016-06-22 11:20:40 -0700

    added game shaming

--- app/components/user-gamelibrary.js
+++ app/components/user-gamelibrary.js
@@ -3,4 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   sortBy: ['playtime_forever:desc'],
   sortedGames: Ember.computed.sort('model.games', 'sortBy'),
+  unplayedGames: Ember.computed('mode.games', function() {
+    var total = this.get('model.games').length
+    var unplayed = 0;
+    this.get('model.games').forEach(function(game) {
+      if (game.playtime_forever === 0) {
+        unplayed++;
+      }
+    });
+    return 'you have ' + unplayed  + ' unplayed games out of ' + total + ".  That's " + (100*unplayed/total).toFixed(2) + "% of your games. For shame!";
+  }),
 });
