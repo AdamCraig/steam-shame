c6405205d0fb6141d08196da220a59615ea5e94b
Noah Kittleson <noahkittleson@gmail.com>
2016-06-22 14:50:08 -0700

    minor styling changes

--- app/components/user-gamelibrary.js
+++ app/components/user-gamelibrary.js
@@ -3,16 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   sortBy: ['playtime_forever:desc'],
   sortedGames: Ember.computed.sort('model.games', 'sortBy'),
-  // unplayedGames: Ember.computed('model.games', function() {
-  //   var total = this.get('model.games').length
-  //   var unplayed = 0;
-  //   this.get('model.games').forEach(function(game) {
-  //     if (game.playtime_forever === 0) {
-  //       unplayed++;
-  //     }
-  //   });
-  //   return 'you have ' + unplayed  + ' unplayed games out of ' + total + ".  That's " + (100*unplayed/total).toFixed(2) + "% of your games. For shame!  You have spend a total of " + (totalTime/1440).toFixed(2) + " days on Steam";
-  // }),
   totalGameTime: Ember.computed('model.games', function() {
     var totalTime = 0;
     this.get('model.games').forEach(function(game) {
