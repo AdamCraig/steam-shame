85dac7241a4ade8712ef2a1a93cfe302f89ff84a
Adam Craig and Ashley Maceli <adam@>
2016-06-23 09:31:06 -0700

    Fix asynchrony in game-search to wait to transitionTo game-results until after data from API is finished loading

--- app/routes/game-search.js
+++ app/routes/game-search.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     gameSearch(params) {
+      var self = this;
       var allGamesURL = 'http://localhost:4000/steam/games';
       var appID = Ember.$.getJSON(allGamesURL).then(function(response) {
         var list = response.applist.apps.app;
@@ -14,11 +15,11 @@ export default Ember.Route.extend({
               foundGameID = item.appid;
             }
           });
-          return foundGameID;
+          self.transitionTo('game-results', foundGameID);
         };
         return getGameID(params.game);
       });
-      this.transitionTo('game-results', appID);
+
     },
 
   }
