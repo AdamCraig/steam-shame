ebc8a429ba6a863f1f835048935c549e2d4901c9
Noah Kittleson <noahkittleson@gmail.com>
2016-06-23 10:31:56 -0700

    objectively improved the all-friends route

--- app/routes/all-friends.js
+++ app/routes/all-friends.js
@@ -3,10 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     var url = 'http://localhost:4000/steam/' + params.userid + '/friends';
+
     return Ember.$.getJSON(url).then(function(responseJSON) {
-      console.log("all friends: ");
-      console.log(responseJSON.friendslist.friends);
-      return responseJSON.friendslist.friends;
+      var idString = 'http://localhost:4000/steam/user/';
+      for (var i = 0; i < responseJSON.friendslist.friends.length; i++) {
+        idString = idString + responseJSON.friendslist.friends[i].steamid + ',';
+      }
+      idString = idString.substring(0, idString.length - 1) + '/profile';
+      return Ember.$.getJSON(idString).then(function(APIresponse) {
+        console.log(APIresponse.response.players);
+        return APIresponse.response.players;
+      });
+
     });
   }
 });
