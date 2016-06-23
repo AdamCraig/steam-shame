f35da56b770e0bc26eabfe5a15de75339e08c582
Adam Craig and Ashley Maceli <ashley@>
2016-06-23 09:37:34 -0700

    Add error checking for game search so that an error message is displayed if a user entry has no matches

--- app/routes/game-search.js
+++ app/routes/game-search.js
@@ -7,7 +7,7 @@ export default Ember.Route.extend({
       var allGamesURL = 'http://localhost:4000/steam/games';
       var appID = Ember.$.getJSON(allGamesURL).then(function(response) {
         var list = response.applist.apps.app;
-        var foundGameID;
+        var foundGameID = "No Match";
 
         var getGameID = function(title){
           list.forEach(function(item) {
@@ -15,7 +15,11 @@ export default Ember.Route.extend({
               foundGameID = item.appid;
             }
           });
-          self.transitionTo('game-results', foundGameID);
+          if (foundGameID === "No Match") {
+            alert("No match found. Try again!");
+          } else {
+            self.transitionTo('game-results', foundGameID);
+          }  
         };
         return getGameID(params.game);
       });
